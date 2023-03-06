d8788316037bc6f0cf21dcda412e8fd82efe7476
Do Yoon Jung <jungdo8016@gmail.com>
2023-03-06 18:27:18 +0900

    Fix

--- src/Pragrammers/Level2/MaxAndMinValues.java
+++ src/Pragrammers/Level2/MaxAndMinValues.java
@@ -6,7 +6,6 @@ public class MaxAndMinValues {
 
     public static void main(String[] args) {
         String s = "1 2 3 4";
-
         String[] temp = s.split(" ");
         int[] arr = new int[temp.length];
 
